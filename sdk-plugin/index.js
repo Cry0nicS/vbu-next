eaf3720bcb77c2be4f724e94a951eb1da7d1c273
Adrian Popescu <adrian@gosugamers.net>
2022-09-13 15:41:12 +0200

    Add live preview

--- sdk-plugin/index.js
+++ sdk-plugin/index.js
@@ -1,15 +1,41 @@
-import * as contentstack from "contentstack";
-import * as Utils from "@contentstack/utils";
+import * as contentstack from 'contentstack';
+import * as Utils from '@contentstack/utils';
+
+import ContentstackLivePreview from '@contentstack/live-preview-utils';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+const envConfig = process.env.CONTENTSTACK_API_KEY
+  ? process.env
+  : publicRuntimeConfig;
 
 const Stack = contentstack.Stack({
-  api_key: process.env.CONTENTSTACK_API_KEY,
-  delivery_token: process.env.CONTENTSTACK_DELIVERY_TOKEN,
-  environment: process.env.CONTENTSTACK_ENVIRONMENT
+  api_key: envConfig.CONTENTSTACK_API_KEY
+    ? envConfig.CONTENTSTACK_API_KEY
+    : envConfig.NEXT_PUBLIC_CONTENTSTACK_API_KEY,
+  delivery_token: envConfig.CONTENTSTACK_DELIVERY_TOKEN,
+  environment: envConfig.CONTENTSTACK_ENVIRONMENT,
+  region: envConfig.CONTENTSTACK_REGION ? envConfig.CONTENTSTACK_REGION : 'us',
+  live_preview: {
+    enable: true,
+    management_token: envConfig.CONTENTSTACK_MANAGEMENT_TOKEN,
+    host: envConfig.CONTENTSTACK_API_HOST,
+  },
 });
 
-if (process.env.CONTENTSTACK_API_HOST) {
-  Stack.setHost(process.env.CONTENTSTACK_API_HOST);
+if (envConfig.CONTENTSTACK_API_HOST) {
+  Stack.setHost(envConfig.CONTENTSTACK_API_HOST);
 }
+
+ContentstackLivePreview.init({
+  stackSdk: Stack,
+  clientUrlParams: {
+    host: envConfig.CONTENTSTACK_APP_HOST,
+  },
+  ssr: false,
+});
+
+export const { onEntryChange } = ContentstackLivePreview;
 
 const renderOption = {
   ["span"]: (node, next) => {
